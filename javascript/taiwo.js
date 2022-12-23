66c7039aa55421a0ad45cccc09687fdb93e9da99
dakerel <121304972+dakerel@users.noreply.github.com>
2022-12-23 16:51:27 +0100

    boom
    i don't know

---
+++ javascript/taiwo.js
@@ -0,0 +1,12 @@
+// let firstName = "Taiwo";
+// let lastName = "Akerele";
+// let fullName = firstName +" " + lastName;
+// console.log(fullName);
+let firstName = "linda"
+let lastName ="Akerele"
+
+function calledOut() {
+    console.log("Hi there, " + firstName);
+    
+}
+calledOut(3)
